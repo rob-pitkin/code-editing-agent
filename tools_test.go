64a9e1f8caa5b294e9c850a17e966303da1bb134
Rob Pitkin <43574523+rob-pitkin@users.noreply.github.com>
2025-07-31 08:21:27 -0700

    add tests for editing

--- tools_test.go
+++ tools_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -101,6 +102,107 @@ func TestReadLines(t *testing.T) {
 		"end_line":   2
 	}`)
 	_, err = ReadLines(readLinesInput)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+}
+
+func TestEditFile(t *testing.T) {
+	// create a test file for editing
+	os.WriteFile("test_edit.txt", []byte("test1\ntest2\ntest3\ntest4\ntest5"), 0644)
+	defer os.Remove("test_edit.txt")
+
+	// happy path
+	editFileInput := json.RawMessage(`{
+		"path": "test_edit.txt",
+		"old_str": "test1",
+		"new_str": "test10"
+	}`)
+	result, err := EditFile(editFileInput)
+	if err != nil {
+		t.Fatalf("failed to edit file: %v", err)
+	}
+	if result != "OK" {
+		t.Fatalf("expected OK, got %s", result)
+	}
+	fileContent, err := os.ReadFile("test_edit.txt")
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+	if string(fileContent) != "test10\ntest2\ntest3\ntest4\ntest5" {
+		t.Fatalf("expected test10, got %s", string(fileContent))
+	}
+
+	// test file doesn't exist
+	editFileInput = json.RawMessage(`{
+		"path": "test_edit2.txt",
+		"old_str": "test1",
+		"new_str": "test10"
+	}`)
+	_, err = EditFile(editFileInput)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	// test file doesn't exist and old_str is empty --> create file
+	editFileInput = json.RawMessage(`{
+		"path": "test_edit2.txt",
+		"old_str": "",
+		"new_str": "test123"
+	}`)
+	_, err = EditFile(editFileInput)
+	if err != nil {
+		t.Fatalf("failed to edit file: %v", err)
+	}
+	fileContent, err = os.ReadFile("test_edit2.txt")
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+	if string(fileContent) != "test123" {
+		t.Fatalf("expected test123, got %s", string(fileContent))
+	}
+	defer os.Remove("test_edit2.txt")
+
+	// test old_str doesn't match exactly once
+	editFileInput = json.RawMessage(`{
+		"path": "test_edit.txt",
+		"old_str": "test",
+		"new_str": "test99"
+	}`)
+	_, err = EditFile(editFileInput)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	// test old_str and new_str are the same
+	editFileInput = json.RawMessage(`{
+		"path": "test_edit.txt",
+		"old_str": "test1",
+		"new_str": "test1"
+	}`)
+	_, err = EditFile(editFileInput)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	// test old_str is empty, but file exists
+	editFileInput = json.RawMessage(`{
+		"path": "test_edit.txt",
+		"old_str": "",
+		"new_str": "test1"
+	}`)
+	_, err = EditFile(editFileInput)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	// test empty file path
+	editFileInput = json.RawMessage(`{
+		"path": "",
+		"old_str": "test1",
+		"new_str": "test10"
+	}`)
+	_, err = EditFile(editFileInput)
 	if err == nil {
 		t.Fatalf("expected error, got nil")
 	}
